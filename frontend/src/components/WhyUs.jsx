c0f26069e1a5f3376db0dddb06e227bdaced3f9c
Jack <namelockcaj@gmail.com>
2024-03-08 12:21:31 +0000

    style add curve divider to why us section

--- frontend/src/components/WhyUs.jsx
+++ frontend/src/components/WhyUs.jsx
@@ -5,7 +5,12 @@ import Image_Beekeeper from '../assets/beekeeper.jpg'
 const WhyUs = () => {
     return ( 
         <section className="why-us-section">
-            <h1>Why Us?</h1>
+
+            <div className="custom-shape-divider-top-1709899883">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                </svg>
+            </div>
             
             <div className="why-us-card">
                 <div className="why-us-text">
@@ -21,6 +26,12 @@ const WhyUs = () => {
                     <p>We prioritize eco-friendly methods and habitat preservation to ensure the well-being of our vital pollinators. With Honey Haven, you contribute to the protection of bee populations and the sustainability of our ecosystems.</p>    
                 </div>
                 <img src={Image_Beekeeper} alt="" className="why-us-img" />
+            </div>
+
+            <div className="custom-shape-divider-bottom-1709899597">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                </svg>
             </div>
         </section>
      );
